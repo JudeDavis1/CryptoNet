ed9f540a3ee73383ddc9088db53c40ec4e24c8a2
Jude Davis <judedavis92@gmail.com>
2020-11-26 19:27:46 +0000

    Started writing Server

--- server.py
+++ server.py
@@ -3,9 +3,19 @@ import socket
 
 class Server:
 
-    def __init__(self, socket, host, port):
-        self.socket = socket
+    def __init__(self, sock, host, port):
+        self.sock = sock
+        self.host = host
+        self.port = port
+
+
+    def run(self):
+        self.sock.bind((self.host, self.port))
+        self.sock.listen(5)  # start listening
+
+        while True:
+            c, addr = self.sock.accept()  # retrieve connection and address when a connection is accepted
 
 
 if __name__ == "__main__":
-    main()
+    server = Server(socket.socket(), "", 3456)
